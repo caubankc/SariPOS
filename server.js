7c05e606b53835c9650c967fdaae2c59657e9abf
ubankconnect <christian@ubankconnect.com>
2022-11-10 02:51:06 +0800

    log visitor geolocation

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const productRouter = require('./routes/productRoutes.js');
+const MongoClient = require('mongodb').MongoClient;
 
 // require('colors');
 
@@ -33,6 +34,21 @@ app.use("/api/products/:key?/:value?", (req, res) => {
     //console.log(req.key);
     productRouter(req, res);
 });
+
+app.use("/api/visits", (req, res) => {
+    const client = new MongoClient(process.env.MONGODB_URI);
+    async function run() {
+        try {
+            const database = client.db();
+            const visits = database.collection("visits");
+            const result = await visits.insertOne(req.body);
+            console.log(`Visitor details recorded: ${result.insertedId}`);
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+})
 
 // create port
 const PORT = process.env.PORT || 5000;
